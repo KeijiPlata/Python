5be65208e67b0a6a300c9b2b7f42b5832157ae08
KeijiPlata <lordmiackykeijip@gmail.com>
2022-12-07 01:24:39 +0800

    Adding Activity 8-6 City Names

---
+++ ReviewPython/act8_cityNames.py
@@ -0,0 +1,9 @@
+# Activity 8-6 City Names
+
+# function that returns city and Country
+def city_country(city, country):
+    """Return city and country"""
+    print(f"{city.title()}, {country.title()}")
+
+# calling the function and passing the arguments.
+city_country("Manila", "Phlippines")
