714b5ede13d7283def201ff7efc80a225a2300ce
KeijiPlata <lordmiackykeijip@gmail.com>
2022-12-07 01:54:15 +0800

    Change function calls so it can print, and store the value into variable

--- ReviewPython/act8_album.py
+++ ReviewPython/act8_album.py
@@ -3,16 +3,23 @@
 def make_album(artistName, albumTitle, numberOfSongs=None):
     """ This function will store information to dictionary"""
     # store the value inside the dictionary
-    album = {'ArtistName' : artistName, 'AlbumTitle': albumTitle}
-
+    album = {
+        'ArtistName' : artistName, 
+        'AlbumTitle': albumTitle
+        }
     # condtion if the user will input the number of songs
     if numberOfSongs:
         album['NumberOfSongs'] = numberOfSongs
 
-    # return the actual dictionary
+    # return the actual dictionary but doesn't give an output
     return album
     
 # function calls
-make_album("Joji", "Nectar")
-make_album("Post Malone", "Stoney")
-make_album("Ed Sheeran", "x", 12)
+print(make_album("Joji", "Nectar"))
+print(make_album("Post Malone", "Stoney"))
+print(make_album("Ed Sheeran", "x", 12))
+
+# you can also store it in a variable and print it
+PostMalone = make_album("Post Malone", "Stoney")
+print(PostMalone)
+
