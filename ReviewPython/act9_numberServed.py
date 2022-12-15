cb3ec4b63d0034570a5469c66cfdcfd251ec3f11
KeijiPlata <lordmiackykeijip@gmail.com>
2022-12-16 02:46:29 +0800

    Adding Activity 9-4 Number Served

---
+++ ReviewPython/act9_numberServed.py
@@ -0,0 +1,33 @@
+# Activity 9-4 Number Served
+# Started at activity 9-1
+
+# Creating a class called Restaurant()
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        """ Initialization of attributes"""
+        self.name = restaurant_name
+        self.ctype = cuisine_type
+        # Adding a attribute aclled number_served with a default value 0
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        """ print the output for attributes"""
+        print(f"Restaurant Name: {self.name.title()}")
+        print(f"Cuisine Type: {self.ctype.title()}")
+        print(f"Number Served: {self.number_served}")
+
+    def open_restaurant(self):
+        """ Prints a message that the restaurant is open """
+        print(f"{self.name.title()} is now open!")
+
+    def set_number_served(self, userinput):
+        """ Let user set the number for how many is served """
+        self.number_served = userinput
+
+    def increment_number_served(self, userinput):
+        """ This will increment the value of number served"""
+        self.number_served += userinput
+
+# creating the instance 
+restaurant = Restaurant("Jollibee", "Fastfood")
+
