51375c19f69e8ef48eca1501467f7509181cf438
KeijiPlata <lordmiackykeijip@gmail.com>
2023-01-12 01:22:57 +0800

    Adding Activity 10-9 Silent Cats and Dogs

---
+++ ReviewPython/act10_silentCatsAndDogs.py
@@ -0,0 +1,20 @@
+# Activty 10-9 Silent Cats and dogs
+# copy of Activity 10-8 Cats and Dogs
+
+def readFile(filepath):
+    """ Read the file and print the names """
+    try:
+     with open(filepath) as f:
+        names = f.read()
+
+    except FileNotFoundError:
+        pass # skips the error and continue the loop
+
+    else:
+        print(names)
+
+# filepaths store in list. snake.txt doesn't exist so we can check if 
+# except block is working
+filepaths = ['ReviewPython\\cats.txt', 'ReviewPython\\dogs.txt', 'ReviewPython\\snake.txt']
+for filepath in filepaths:
+    readFile(filepath)
