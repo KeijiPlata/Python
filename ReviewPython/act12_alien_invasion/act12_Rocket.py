e0b15145d9166dc8757b810c0103fd15f572f83e
KeijiPlata <lordmiackykeijip@gmail.com>
2023-02-21 02:41:31 +0800

    Add limitation to ship so it doens't disappear in the screen

--- ReviewPython/act12_alien_invasion/act12_Rocket.py
+++ ReviewPython/act12_alien_invasion/act12_Rocket.py
@@ -35,13 +35,13 @@ class Rocketgame:
         self.moving_down = False
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.rect.x += 1
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.rect.x -= 1
-        if self.moving_up:
+        if self.moving_up and self.rect.top > 0:
             self.rect.y -= 1
-        if self.moving_down:
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.rect.y += 1
 
     def blitme(self):
