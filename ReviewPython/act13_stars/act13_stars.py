5ab7454131c5fc0106ac256c2e669d59de44a445
KeijiPlata <lordmiackykeijip@gmail.com>
2023-03-06 16:19:03 +0800

    Add row of stars

--- ReviewPython/act13_stars/act13_stars.py
+++ ReviewPython/act13_stars/act13_stars.py
@@ -23,7 +23,12 @@ class StarsGame():
     def __init__(self):
         """Initialize the game"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+
+        # set the size of the screen
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.screen = pygame.display.set_mode((self.screen_width, 
+                                               self.screen_height))
         pygame.display.set_caption("Stars")
 
         # bg color
@@ -34,8 +39,25 @@ class StarsGame():
         self._create_fleet()
 
     def _create_fleet(self):
+        # get the width of the star
         stars = Stars(self)
-        self.stars.add(stars)
+
+        # get the width of the star
+        stars_width = stars.rect.width
+
+        # compute for the available space to compute the number of 
+        # stars possible in the screen
+
+        available_space_x = self.screen_width - (2 * stars_width)
+        number_stars_x = available_space_x // (2 * stars_width)
+
+        for starNum in range(number_stars_x):
+            # create a star and then group it 
+            stars = Stars(self)
+            stars.x = stars_width + 2 * stars_width * starNum
+            stars.rect.x = stars.x
+            self.stars.add(stars)
+
 
     def run_game(self):
         """Main loop"""
